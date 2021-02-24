798202db872b3043dc2a0722886ee7fb8c06fe0f
DESKTOP-ROER1NG\まっちゃん <ilovegames.ilikesplatoon@gmail.com>
2021-02-25 02:54:30 +0900

    平行移動用関数　回転する移動用関数　弾を発射したかどうかのフラグ変数作成

--- Source/player.h
+++ Source/player.h
@@ -10,6 +10,9 @@ public:
 	void Draw() override;
 
 private:
+	void TranslateMove(MATRIX rotationY);//平行移動の関数
+	void RotationMove(MATRIX rotationY);//回転をする移動の関数
+	bool isShot = false;//自機が弾を撃ったらtrue
 	int hModel;//モデルのハンドラー
 	VECTOR position;//プレイヤーの座標
 	VECTOR rotation;//プレイヤーの向き
